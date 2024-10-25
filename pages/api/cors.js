82f52199e1e0f0543a40dc1f122cafc7963923e7
Mohamed Ebrahem <aalmsidy1@gmail.com>
2024-10-25 17:40:47 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   // Set CORS headers to allow any origin
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.okx.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
